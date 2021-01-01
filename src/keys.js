99ca795135a4efa425d3aa2cccbc091b9e97176a
Alan Luna <aklt1994@gmail.com>
2021-01-01 22:46:33 +0100

    first commit

---
+++ src/keys.js
@@ -0,0 +1,12 @@
+/* Aqui se almacenaran las posibles claves o palabras claves para usar el servicio:
+numero de puerto, la conexion a la base de datos, contraseña 
+*/
+
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '1234',
+        database: 'database_links'
+    }
+};
