bfa1dc6a391bf8e62593a68ee7aceee60b58988a
Alan Luna <aklt1994@gmail.com>
2021-01-02 21:01:33 +0100

    commit 3

--- src/routes/links.js
+++ src/routes/links.js
@@ -5,7 +5,7 @@ const pool = require('../database');
 
 router.get('/add', (req, res) => {
     res.render('links/add');
-})
+});
 
 router.post('/add', async (req, res) => {
     const { title, url, description } = req.body;
@@ -16,11 +16,35 @@ router.post('/add', async (req, res) => {
     };
     await pool.query('INSERT INTO links set ?', [newLink]);
     res.redirect('/links');
-})
+});
 
 router.get('/', async (req, res) => {
     const links = await pool.query('SELECT * FROM links');
-    res.render('links/list', {links: links})
+    res.render('links/list', {links: links});
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    pool.query('DELETE FROM links WHERE id = ?', [id]);
+    res.redirect('/links');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const link = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+    res.render('links/edit', {link: link[0]});
+})
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, description, url } = req.body;
+    const newLink = {
+        title,
+        description,
+        url
+    };
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id])
+    res.redirect('/links');
 });
 
 module.exports = router;
