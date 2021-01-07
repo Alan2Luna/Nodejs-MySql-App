297a765aa36fee94f535ce1252f83d05814a9e9b
Alan Luna <aklt1994@gmail.com>
2021-01-07 02:37:45 +0100

    commit signin

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,29 +2,37 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth')
 
-router.get('/signup', (req, res) => {
+router.get('/signup', isNotLoggedIn, (req, res) => {
     res.render('auth/signup');
 });
 
-// Abajo se encuentra una mejor forma de escribirlo
-// router.post('/signup', (req, res) => {
-//     passport.authenticate('local.signup', {
-//         successRedirect: '/profile',
-//         failureRedirect: '/signup',
-//         failureFlash: true
-//     });
-//     res.send('received');
-// });
-
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup', isNotLoggedIn, passport.authenticate('local.signup', {
     successRedirect: '/profile',
     failureRedirect: '/signup',
     failureFlash: true
 }))
 
-router.get('/profile', (req, res)  => {
-    res.send('this is your profile');
+router.get('/signin', isNotLoggedIn, (req, res)  => {
+    res.render('auth/signin');
 });
+
+router.post('/signin', isNotLoggedIn, async (req, res, next) => {
+    passport.authenticate('local.signin', {
+        successRedirect: '/profile',
+        failureRedirect: '/signin',
+        failureFlash: true
+    })(req, res, next);
+});
+
+router.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile');
+});
+
+router.get('/logout', (req,res) => {
+    req.logOut();
+    res.redirect('/signin');
+})
 
 module.exports = router;
